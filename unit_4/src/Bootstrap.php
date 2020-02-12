d35f8221dd83515a35a52e122552c6e6559e2274
David Petter Alves <cfdavidpetter@gmail.com>
2020-02-12 07:58:02 -0300

    Testing whoops

--- unit_4/src/Bootstrap.php
+++ unit_4/src/Bootstrap.php
@@ -2,4 +2,13 @@
 
 declare(strict_types = 1);
 
-echo 'Begin Application';
+require __DIR__ . '/../vendor/autoload.php';
+
+//PHP errors for cool kids
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+
+
+throw new Exception("Error Processing Request", 1);
